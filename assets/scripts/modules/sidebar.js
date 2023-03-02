7a7bb2ff7b97537ba2dbf165c42ece8df6ac9e39
FlorencioGoncalves <florenciopaul@gmail.com>
2023-03-02 12:38:16 +0100

    Tela | Transmissão - 85%

--- assets/scripts/modules/sidebar.js
+++ assets/scripts/modules/sidebar.js
@@ -1,13 +1,13 @@
-export default function sideBar(){
-    const sidebar = document.querySelector('.nav-menu')
-    const toggle = document.querySelector('.nav-menu .user-bar')
+// export default function sideBar(){
+//     const sidebar = document.querySelector('.nav-menu')
+//     const toggle = document.querySelector('.nav-menu .user-bar')
 
-    toggle.addEventListener('click', () => {
-      sidebar.classList.toggle('_expanded')
-    })
+//     toggle.addEventListener('click', () => {
+//       sidebar.classList.toggle('_expanded')
+//     })
 
-    document.addEventListener('click', (e) => {
-      if(!sidebar.contains(e.target))
-        sidebar.classList.remove('_expanded')
-    })
-}
+//     document.addEventListener('click', (e) => {
+//       if(!sidebar.contains(e.target))
+//         sidebar.classList.remove('_expanded')
+//     })
+// }
